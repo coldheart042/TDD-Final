88480ab4d725d9cfa8df0e0bceba7d2a8463f010
Richard Williams <coldheart042@gmail.com>
2014-03-04 08:21:02 -0800

    Initial Commit
    Created the files

---
+++ puzz.rb
@@ -0,0 +1,10 @@
+class PuzzleClass
+  attr_accessor :width
+  attr_accessor :height
+  def initialize(width,height)
+    @width = width
+    @height = height
+  end
+
+
+end
---
+++ test_puzz.rb
@@ -0,0 +1,3 @@
+require('test/unit')
+require_relative('puzz')
+puzzle = PuzzleClass.new(10,12)
