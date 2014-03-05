340b46cea4346216e0a55ec2eec7daa4b97f3588
Richard Williams <coldheart042@gmail.com>
2014-03-05 08:27:01 -0800

    Rev 2
    Added tests and class methods

--- puzz.rb
+++ puzz.rb
@@ -1,10 +1,16 @@
 class PuzzleClass
-  attr_accessor :width
-  attr_accessor :height
+  attr_accessor :width, :height
+  attr_reader :initial_config
   def initialize(width,height)
     @width = width
     @height = height
   end
 
+  def set_puzzle(config)
+    @initial_config = config
+  end
 
+  def get_puzzle()
+
+  end
 end
--- test_puzz.rb
+++ test_puzz.rb
@@ -1,3 +1,22 @@
-require('test/unit')
+require('minitest/autorun')
 require_relative('puzz')
-puzzle = PuzzleClass.new(10,12)
+class Test_puzz < MiniTest::Test
+  def setup
+    @puzzle = PuzzleClass.new(3,3)
+  end
+
+  def test_init
+    assert_equal(PuzzleClass, @puzzle.class, "Class not created")
+    assert_equal(@puzzle.width, 3, "Width not created")
+  end
+
+  def test_set_puzzle
+    assert_respond_to(@puzzle, :set_puzzle, "Set_Puzzle not written!")
+    @puzzle.set_puzzle('12345 678')
+    assert_equal('12345 678', @puzzle.initial_config, "Initial Config")
+  end
+
+  def test_get_puzzle
+    assert_respond_to(@puzzle, :get_puzzle, "Get_Puzzle not written!")
+  end
+end
