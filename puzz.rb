2086fc33f485eeaabed915af97f1d5cf6b09b7e2
Richard Williams <coldheart042@gmail.com>
2014-03-05 20:59:08 -0800

    Rev 3
    Tests + Methods added: Initialize(), set_puzzle(), get_puzzle(),
    solve(), and to_s

--- puzz.rb
+++ puzz.rb
@@ -1,6 +1,6 @@
 class PuzzleClass
   attr_accessor :width, :height
-  attr_reader :initial_config
+  attr_reader :initial_config, :rows
   def initialize(width,height)
     @width = width
     @height = height
@@ -8,9 +8,49 @@ class PuzzleClass
 
   def set_puzzle(config)
     @initial_config = config
+    @rows = Array.new(height){Array.new(width)}
+    temp = @initial_config.split('')
+    h = 0
+    i = 0
+    until i == height
+      j = 0
+      until j == width
+        @rows[i][j] = temp[h]
+        h += 1
+        j += 1
+      end
+      i += 1
+    end
   end
 
   def get_puzzle()
+    @initial_config
+  end
 
+  def to_s
+    result = ""
+    @rows.each { |row| result += "#{row.join("")}\n" }
+    result.chomp
+  end
+
+  def solve
+    temp = Array.new(height){Array.new(width)}
+    str_temp = @initial_config.split('').sort!
+    h = 0
+    i = 0
+    until i == height
+      j = 0
+      until j == width
+        temp[i][j] = str_temp[h]
+        h += 1
+        j += 1
+      end
+      i += 1
+    end
+    str_temp = ""
+    temp.each do |row|
+      str_temp += "#{row.join("")}\n"
+    end
+    str_temp.chomp
   end
 end
--- test_puzz.rb
+++ test_puzz.rb
@@ -1,22 +1,40 @@
 require('minitest/autorun')
 require_relative('puzz')
 class Test_puzz < MiniTest::Test
+
   def setup
     @puzzle = PuzzleClass.new(3,3)
+    @puzzle.set_puzzle("12345*678")
   end
 
   def test_init
-    assert_equal(PuzzleClass, @puzzle.class, "Class not created")
-    assert_equal(@puzzle.width, 3, "Width not created")
+    assert_equal(PuzzleClass, @puzzle.class, "Not a class")
+    assert_equal(@puzzle.width, 3, "Width not set")
+    assert_equal(@puzzle.height, 3, "Height not set")
   end
 
   def test_set_puzzle
-    assert_respond_to(@puzzle, :set_puzzle, "Set_Puzzle not written!")
-    @puzzle.set_puzzle('12345 678')
-    assert_equal('12345 678', @puzzle.initial_config, "Initial Config")
+    assert_respond_to(@puzzle, :set_puzzle, "No method")
+    #@puzzle.set_puzzle('12345 678')
+    assert_equal('12345*678', @puzzle.initial_config, "Initial Config failed")
+    assert_equal(Array, @puzzle.rows.class, "Not an array")
+    assert_equal(3, @puzzle.rows.count, "Incorrect number of sub-arrays")
+    assert_equal([["1","2","3"],["4","5","*"],["6","7","8"]], @puzzle.rows, "Array doesn't match")
   end
 
   def test_get_puzzle
-    assert_respond_to(@puzzle, :get_puzzle, "Get_Puzzle not written!")
+    assert_respond_to(@puzzle, :get_puzzle, "No method 'get_puzzle'")
+    #@puzzle.set_puzzle('12345 678')
+    assert_equal('12345*678', @puzzle.get_puzzle, "Failed to retrieve config")
+  end
+
+  def test_to_s
+    assert_respond_to(@puzzle, :to_s, "No method 'to_s'") # I know... to_s already exists - this is just practice
+    assert_equal("123\n45*\n678", @puzzle.to_s, "Did not return correctly")
+  end
+
+  def test_solve
+    assert_respond_to(@puzzle, :solve, "No method 'solve'")
+    assert_equal("*12\n345\n678", @puzzle.solve, "Did not return solution!")
   end
 end
